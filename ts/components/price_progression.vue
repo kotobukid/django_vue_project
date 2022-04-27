36b7a677c089241a2066aa3dda65a7de07d91f2c
kotobukid <kotobuki@daqua.jp>
2022-04-27 14:02:24 +0900

    親コンポーネントが格納しているデータをSVG上に描画

--- ts/components/price_progression.vue
+++ ts/components/price_progression.vue
@@ -1,9 +1,11 @@
 <template lang="pug">
     .price_progression
         tax-calc(@register-snapshot="register_snapshot")
-        br
-        ul
-            li(v-for="his in price_history") {{ his }}
+        svg.chart(width="1600" height="400" viewBox="0 0 1600 400")
+            g(transform="scale(1, -1)")
+                g(transform="translate(20, -390)")
+                    line.horizon(x1="-20" y1="0" x2="1600" y2="0")
+                    circle.snap(v-for="(h, $index) in price_history" :cx="$index * 10" :cy="Math.round(h.price * ((100 + h.tax_rate) / 100))" r="3")
 </template>
 
 <script lang="ts">
@@ -30,5 +32,22 @@ export default PriceProgression;
 </script>
 
 <style scoped lang="less">
+svg.chart {
+    border: 1px solid grey;
+    background-color: white;
 
+    circle.snap {
+        fill: lightblue;
+        stroke: blue;
+        stroke-width: 1px;
+        vector-effect: non-scaling-stroke;
+    }
+
+    line.horizon {
+        stroke-width: 1px;
+        stroke: black;
+        fill: none;
+        vector-effect: non-scaling-stroke;
+    }
+}
 </style>
