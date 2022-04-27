7ad3a206041f587be1ab17b12f7f57fb007e01ec
kotobukid <kotobuki@daqua.jp>
2022-04-27 12:51:53 +0900

    lessファイルをトップレベルで読み込みたい場合は、メインのtsファイル上でrequireする lessファイルはwebpackにより読み込まれ、index.js内に結合されるため、staticディレクトリのようなブラウザから直接アクセスできる場所に配置する必要はない

--- ts/index.ts
+++ ts/index.ts
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import hoge from './lib/sub';
 import MainApp from './components/main_app.vue';
 
+require('../less/common.less');
+
 window.onload = () => {
     const vm = new Vue({
         components: {
