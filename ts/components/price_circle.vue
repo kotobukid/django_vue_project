ba15788a970ad167d7f4dcc6d38db55c2d3476fd
kotobukid <kotobuki@daqua.jp>
2022-04-27 15:28:51 +0900

    円の詳細情報をマウスホバーで参照可能に

--- ts/components/price_circle.vue
+++ ts/components/price_circle.vue
@@ -1,6 +1,15 @@
 <template lang="pug">
-    g.snap
+    g.snap(
+        @pointerenter="p_enter"
+        @pointerleave="p_leave"
+    )
         circle.snap(:cx="index * 10" :cy="tax_computed" r="3")
+        g.tip(:transform="tip_translate" v-if="show_tips")
+            g(style="transform: scale(1, -1)")
+                path(d="M 62.764 12.122 L 62.764 52.38 L 11.132 52.38 L 11.132 23.476 L 3.51 3.593 L 17.635 12.122 L 62.764 12.122 Z" style="fill: rgb(216, 216, 216); stroke: rgb(0, 0, 0);")
+                text.price(x="35.719" y="23.394" v-text="`￥${price}`")
+                text.tax_rate(x="35.719" y="36.486" v-text="`+ ${tax_rate}%`")
+                text.total(x="35.719" y="49.577" v-text="`￥${tax_computed}`")
 
 </template>
 
@@ -25,6 +34,20 @@ class PriceCircle extends Vue {
     get tax_computed(): number {
         return calc_tax(this.price, this.tax_rate);
     }
+
+    get tip_translate(): string {
+        return `translate(${this.index * 10} ${this.tax_computed})`;
+    }
+
+    show_tips: boolean = false;
+
+    p_enter() {
+        this.show_tips = true;
+    }
+
+    p_leave() {
+        this.show_tips = false;
+    }
 }
 
 export default PriceCircle;
@@ -38,5 +61,19 @@ circle.snap {
     stroke: red;
     stroke-width: 1px;
     vector-effect: non-scaling-stroke;
+}
+
+g.tip {
+    pointer-events: none;
+}
+
+text {
+    &.tax_rate, &.price, &.total {
+        white-space: pre;
+        fill: rgb(51, 51, 51);
+        font-family: Arial, sans-serif;
+        font-size: 12.2px;
+        text-anchor: middle;
+    }
 }
 </style>
